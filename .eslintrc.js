7a17a0d076a31b7cbd0cc759bfd8337a55c0081f
Ben Hide <ben.m.hide@googlemail.com>
2021-03-02 17:36:52 +0000

    Initial commit - setup of dev env / linting and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ['airbnb-typescript', 'react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+};
